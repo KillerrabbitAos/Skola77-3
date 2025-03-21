a870399a61ed83917293a0532736009c36388f50
Centiljard <89267105+Centiljard@users.noreply.github.com>
2025-03-22 00:38:12 +0100

    Refactor Home page logic and add Settings page.
    Simplified rendering logic in the Home page by introducing a dedicated `renderContent` function. Updated text label in `ThemeSwitcher` and adjusted global text color for improved styling. Added a new Settings page with theme-switching capabilities

--- src/app/components/profile.tsx
+++ src/app/components/profile.tsx
@@ -10,6 +10,7 @@ export function Profile(props: {
     function redirectToSettings(){
         window.location.href = "/settings";
     }
+    // eslint-disable-next-line @next/next/no-img-element
     return <img onClick={redirectToSettings} className={"border-black dark:border-white border-3 rounded-full size-14 m-2  "}
                 src={props.user?.image as string} alt=":)"/>;
 }
