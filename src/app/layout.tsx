54d158e1944029cd388427a36b1958d7cb777a8d
Centiljard <89267105+Centiljard@users.noreply.github.com>
2025-03-25 21:30:04 +0100

    Add site preview meta tag
    It's for SEO.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,9 @@ export default function RootLayout({
             <title>Skola77-3</title>
             <meta property="description" content="Skola77-3 alpha preview site"/>
             <meta property="og:title" content="Skola77-3 pre-alpha"/>
-            <meta property="og:description" content="Preview for those who are interested in the third version of Skola77, which is still only in the first steps of development." />
+            <meta property="og:site_name" content="Skola77-3 first alpha preview"/>
+            <meta property="og:description"
+                  content="Preview for those who are interested in the third version of Skola77, which is still only in the first steps of development."/>
         </head>
         <body>
         <Providers>{children}</Providers>
