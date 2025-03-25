c6d1f0b791afc192e676984a4b4d587a7c42f25e
Centiljard <89267105+Centiljard@users.noreply.github.com>
2025-03-25 21:03:04 +0100

    ´Patch migrations even more´
    I tinkered with them before.

--- prisma/migrations/20250325190846_change_location_to_point/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to alter the column `location` on the `User` table. The data in that column could be lost. The data in that column will be cast from `Text` to `Unsupported("geometry(Point, 4326)")`.
-  - Made the column `location` on table `User` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE "User" DROP COLUMN "location"
