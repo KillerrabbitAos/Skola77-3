7179d1b02742cb43406a1ef5284daa9748ae8ba3
Centiljard <89267105+Centiljard@users.noreply.github.com>
2025-03-25 17:29:54 +0100

    Wrap route functions to prevent their loss in building

--- src/app/api/groups/route.ts
+++ src/app/api/groups/route.ts
@@ -1,8 +1,25 @@
-import {GET} from "@/app/api/groups/get";
-import {POST} from "@/app/api/groups/post";
-import {DELETE} from "@/app/api/groups/delete";
-import {PUT} from "@/app/api/groups/put";
-import {PATCH} from "@/app/api/groups/patch";
+import { GET as getHandler } from './get';
+import { PATCH as patchHandler } from './patch';
+import { POST as postHandler } from './post';
+import { DELETE as deleteHandler } from './delete';
+import { PUT as putHandler } from './put';
 
+export async function GET(req: Request) {
+    return getHandler(req);
+}
 
-export {GET, POST, DELETE, PUT, PATCH}
+export async function POST(req: Request) {
+    return postHandler(req);
+}
+
+export async function PATCH(){
+    return patchHandler()
+}
+
+export async function DELETE(req: Request){
+    return deleteHandler(req)
+}
+
+export async function PUT(req: Request){
+    return putHandler(req)
+}
