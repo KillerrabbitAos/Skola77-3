4cdd0af21245b3e1bf49cb2d14a4f7ec003b98b5
Centiljard <89267105+Centiljard@users.noreply.github.com>
2025-04-08 18:29:43 +0200

    add background color etc to creat-group button for classic theme

--- src/app/create-group/page.tsx
+++ src/app/create-group/page.tsx
@@ -1,53 +1,64 @@
-"use client"
+"use client";
 
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from "react";
 
 function Page() {
-    const [mounted, setMounted] = useState(false);
-    const [groupName, setGroupName] = useState('');
-    const [height, setHeight] = useState(window.innerHeight);
+  const [mounted, setMounted] = useState(false);
+  const [groupName, setGroupName] = useState("");
+  const [height, setHeight] = useState(window.innerHeight);
 
-    async function createGroup() {
-        try {
-            const response = await fetch('/api/groups', {
-                method: 'POST',
-                body: JSON.stringify({name: groupName}),
-            });
-            const data = await response.json();
-            const id = data?.id
-            if (!id) return;
-            window.location.replace('/group?id=' + String(id) + '')
-        } catch (e) {
-            console.error(e)
-        }
+  async function createGroup() {
+    try {
+      const response = await fetch("/api/groups", {
+        method: "POST",
+        body: JSON.stringify({ name: groupName }),
+      });
+      const data = await response.json();
+      const id = data?.id;
+      if (!id) return;
+      window.location.replace("/group?id=" + String(id) + "");
+    } catch (e) {
+      console.error(e);
     }
+  }
 
-    useEffect(() => {
-        setMounted(true);
-    }, []);
-    useEffect(() => {
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+  useEffect(() => {
+    const updateHeight = () => setHeight(window.innerHeight);
+    window.addEventListener("resize", updateHeight);
+    return () => window.removeEventListener("resize", updateHeight);
+  }, []);
 
+  if (!mounted) return null;
 
-        const updateHeight = () => setHeight(window.innerHeight);
-        window.addEventListener("resize", updateHeight);
-        return () => window.removeEventListener("resize", updateHeight);
-    }, []);
-
-    if (!mounted) return null;
-
-    return (
-        <div style={{position: "absolute", top: height / 2, left: "50%", transform: "translate(-50%, -50%)"}}>
-            <div
-                className="flex flex-col items-center gap-2 dark:bg-gray-700 light:bg-[lightgray] p-4 rounded-2xl dark:border-1 ">
-                <input type="text" placeholder="Group name" onChange={(e) => setGroupName(e.target.value)}
-                       className="p-2 dark:border rounded"
-                       style={{outline: 'none'}}/>
-                <button onClick={createGroup}
-                        className="cursor-pointer w-full light:text-gray-700 p-2 border-1 dark:text-white  rounded">Create
-                </button>
-            </div>
-        </div>
-    );
+  return (
+    <div
+      style={{
+        position: "absolute",
+        top: height / 2,
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+      }}
+    >
+      <div className="flex flex-col items-center gap-2 dark:bg-gray-700 light:bg-[lightgray] classic:bg-gray-200 p-4 rounded-2xl dark:border-1 ">
+        <input
+          type="text"
+          placeholder="Group name"
+          onChange={(e) => setGroupName(e.target.value)}
+          className="p-2 dark:border rounded"
+          style={{ outline: "none" }}
+        />
+        <button
+          onClick={createGroup}
+          className="cursor-pointer w-full classic:bg-green-500 classic:text-white light:text-gray-700 p-2 border-1 dark:text-white classic:hover:bg-green-600 rounded"
+        >
+          Create
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Page;
