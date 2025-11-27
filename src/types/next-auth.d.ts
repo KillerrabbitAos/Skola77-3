a8578ca83aa949f94d2f3d7acb19234293fc1e72
Artur Norrby <89267105+Centiljard@users.noreply.github.com>
2025-11-27 10:01:20 +0100

    remove redundant comment

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -3,7 +3,7 @@ import { DefaultSession } from "next-auth";
 declare module "next-auth" {
     interface Session {
         user: {
-            id: string; // add this line to ensure the session user has an 'id' field
+            id: string; 
         } & DefaultSession["user"];
     }
 }
